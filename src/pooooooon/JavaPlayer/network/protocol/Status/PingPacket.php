c455e2a63b1af93699a2fc06c78a9b81faae66f8
kanison konkla <53283017+pooooooon@users.noreply.github.com>
2022-03-28 16:56:45 +0700

    Add files via upload

---
+++ src/pooooooon/JavaPlayer/network/protocol/Status/PingPacket.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ *  ______  __         ______               __    __
+ * |   __ \|__|.-----.|   __ \.----..-----.|  |_ |  |--..-----..----.
+ * |   __ <|  ||  _  ||   __ <|   _||  _  ||   _||     ||  -__||   _|
+ * |______/|__||___  ||______/|__|  |_____||____||__|__||_____||__|
+ *             |_____|
+ *
+ * BigBrother plugin for PocketMine-MP
+ * Copyright (C) 2014-2015 shoghicp <https://github.com/shoghicp/BigBrother>
+ * Copyright (C) 2016- BigBrotherTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @author BigBrotherTeam
+ * @link   https://github.com/BigBrotherTeam/BigBrother
+ *
+ */
+
+declare(strict_types=1);
+
+namespace pooooooon\javaplayer\network\protocol\Status;
+
+use pooooooon\javaplayer\network\Packet;
+
+class PingPacket extends Packet
+{
+
+	/** @var int */
+	public $time;
+
+	public function pid(): int
+	{
+		return 0x01;
+	}
+
+	protected function encode(): void
+	{
+		$this->putLong($this->time);
+	}
+
+	protected function decode(): void
+	{
+		$this->time = $this->getLong();
+	}
+}
