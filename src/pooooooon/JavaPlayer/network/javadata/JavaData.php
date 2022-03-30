fa910139de33fd03becdc601d9933f7d1edd1ab4
kanison konkla <53283017+pooooooon@users.noreply.github.com>
2022-03-30 12:53:40 +0700

    effectid

--- src/pooooooon/JavaPlayer/network/javadata/JavaData.php
+++ src/pooooooon/JavaPlayer/network/javadata/JavaData.php
@@ -92,6 +92,41 @@ class JavaParticleId {
 	public const SCRAPE = 87;
 }
 
+class JavaEffectId {
+	public const SPEED = 1;
+	public const SLOWNESS = 2;
+	public const HASTE = 3;
+	public const MINING_FATIGUE = 4;
+	public const STRENGTH = 5;
+	public const INSTANT_HEALTH = 6;
+	public const INSTANT_DAMAGE = 7;
+	public const JUMP_BOOST = 8;
+	public const NAUSEA = 9;
+	public const REGENERATION = 10;
+	public const RESISTANCE = 11;
+	public const FIRE_RESISTANCE = 12;
+	public const WATER_BREATHING = 13;
+	public const INVISIBILITY = 14;
+	public const BLINDNESS = 15;
+	public const NIGHT_VISION = 16;
+	public const HUNGER = 17;
+	public const WEAKNESS = 18;
+	public const POISON = 19;
+	public const WITHER = 20;
+	public const HEALTH_BOOST = 21;
+	public const ABSORPTION = 22;
+	public const SATURATION = 23;
+	public const GLOWING = 24;
+	public const LEVITATION = 25;
+	public const LUCK = 26;
+	public const UNLUCK = 27;
+	public const SLOW_FALLING = 28;
+	public const CONDUIT_POWER = 29;
+	public const DOLPHINS_GRACE = 30;
+	public const BAD_OMEN = 31;
+	public const HERO_OF_THE_VILLAGE = 32;
+	public const DARKNESS = 33;
+}
 class JavaTileName {
 	public const BANNER = "Banner";
 	public const BED = "Bed";
