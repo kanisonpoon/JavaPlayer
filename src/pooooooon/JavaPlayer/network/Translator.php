d31749a164a211dc912503a76d982aef11d06786
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-05-27 15:00:59 +0300

    Update Translator.php

--- src/pooooooon/JavaPlayer/network/Translator.php
+++ src/pooooooon/JavaPlayer/network/Translator.php
@@ -1,29 +1,4 @@
 <?php
-/**
- *  ______  __         ______               __    __
- * |   __ \|__|.-----.|   __ \.----..-----.|  |_ |  |--..-----..----.
- * |   __ <|  ||  _  ||   __ <|   _||  _  ||   _||     ||  -__||   _|
- * |______/|__||___  ||______/|__|  |_____||____||__|__||_____||__|
- *             |_____|
- *
- * BigBrother plugin for PocketMine-MP
- * Copyright (C) 2014-2015 shoghicp <https://github.com/shoghicp/BigBrother>
- * Copyright (C) 2016- BigBrotherTeam
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * @author BigBrotherTeam
- * @link   https://github.com/BigBrotherTeam/BigBrother
- *
- */
 
 declare(strict_types=1);
 
@@ -2686,7 +2661,7 @@ class Translator
 				$pk->action = ScoreboardObjectivePacket::ACTION_REMOVE;
 				$pk->name = $packet->objectiveName;
 				return $pk;
-			case Info::MODAL_FORM_REQUEST_PACKET:
+			/*case Info::MODAL_FORM_REQUEST_PACKET: check if he is in then send it
 				/** @var ModalFormRequestPacket $packet */
 				$formData = json_decode($packet->formData, true);
 				$packets = [];
@@ -2704,7 +2679,7 @@ class Translator
 					$packets[] = $pk;
 				}
 				$player->bigBrother_formId = $packet->formId;
-				return $packets;
+				return $packets;*/
 			case Info::ADVENTURE_SETTINGS_PACKET:
 				/** @var AdventureSettingsPacket $packet */
 				$canFly = $packet->getFlag($packet::ALLOW_FLIGHT);
